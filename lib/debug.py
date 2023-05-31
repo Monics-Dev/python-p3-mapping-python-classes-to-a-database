290d8d6f01c383e6648e2e84a6fb994040c38bf7
Monica <monijerobon@gmail.com>
2023-05-31 23:05:37 +0300

    completed assignment

--- lib/debug.py
+++ lib/debug.py
@@ -5,4 +5,17 @@ from song import Song
 
 
 if __name__ == '__main__':
+    def create_table(self):
+        sql = """
+            CREATE TABLE IF NOT EXISTS songs (
+                id INTEGER PRIMARY KEY,
+                name TEXT,
+                album TEXT
+            )
+        """
+
+        CURSOR.execute(sql)
+    
+    
+    
     import ipdb; ipdb.set_trace()
